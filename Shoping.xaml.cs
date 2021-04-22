24ac71cf399fbc420d1e248089b59f6d18a24527
Shakhnazar <imramziyev02@gmail.com>
2021-04-23 00:33:09 +0600

    WPFexam Commit

---
+++ Shoping.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Examination_project
+{
+    /// <summary>
+    /// Логика взаимодействия для Shoping.xaml
+    /// </summary>
+    public partial class Shoping : Window
+    {
+        OpenWindow Opw;
+        FirstP Fp;
+        SecondP Sp;
+        ThirdP Tp;
+        FourthP Frp;
+        public Shoping()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Opw = new OpenWindow();
+            Opw.Show();
+            this.Close();
+        }
+
+        private void first_Click(object sender, RoutedEventArgs e)
+        {
+            Fp = new FirstP();
+            Fp.Show();
+        }
+        private void second_Click(object sender, RoutedEventArgs e)
+        {
+            Sp = new SecondP();
+            Sp.Show();
+        }
+        private void third_Click(object sender, RoutedEventArgs e)
+        {
+            Tp = new ThirdP();
+            Tp.Show();
+        }
+        private void fourth_Click(object sender, RoutedEventArgs e)
+        {
+            Frp = new FourthP();
+            Frp.Show();
+        }
+    }
+}
---
+++ registration.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Examination_project
+{
+    /// <summary>
+    /// Логика взаимодействия для registration.xaml
+    /// </summary>
+    public partial class registration : Window
+    {
+        OpenWindow Opw;
+        public registration()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void Registration(object sender, RoutedEventArgs e)
+        {
+            while(TextName1.Text != "" && TextName2.Text != "" && Email.Text != "" && Pass1.Text != "" && Pass2.Text != "")
+            {
+                if (Pass1.Text != Pass2.Text)
+                {
+                    Pass1.Text = "";
+                    Pass2.Text = "";
+                }
+                int str;
+                Pass1.Text = Pass1.Text.Length.ToString();
+                str = Int32.Parse(Pass1.Text);
+                if (str < 8)
+                {
+                    Pass1.Text = "";
+                    Pass2.Text = "";
+                    MessageBox.Show("Пароль не соответсвует норме");
+                    break;
+                }
+                else
+                {
+                    Pass1.Text = "";
+                    Pass2.Text = "";
+                    Opw = new OpenWindow();
+                    Opw.Show();
+                    this.Close();
+                    break;
+                }
+
+            }
+            
+
+
+        }
+    }
+}
